c08f06b935c9ca8d857d90e2f977f4c4a99ca744
Luke Nguyen <luke_nguyen@mckinsey.com>
2022-07-10 20:36:50 +0700

    leetcode-0001-two-sum

---
+++ leetcode_practice/leetcode_practice/leetcode_0001_00_two_sum.java
@@ -0,0 +1,18 @@
+package leetcode_practice;
+
+import java.util.HashMap;
+
+public class leetcode_0001_00_two_sum {
+	public int[] twoSum(int[] numbers, int target) {
+		HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
+		for (int i = 0; i < numbers.length; i++) {
+			Integer diff = (Integer) (target - numbers[i]);
+			if (hash.containsKey(diff)) {
+				int[] toReturn = { hash.get(diff), i };
+				return toReturn;
+			}
+			hash.put(numbers[i], i);
+		}
+		return null;
+	}
+}
