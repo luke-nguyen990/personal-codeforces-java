7e7342a602488bb381f0b2763614d4c11c146b72
Luke Nguyen <luke_nguyen@mckinsey.com>
2022-07-11 21:22:33 +0700

    codeforces-0472A

---
+++ codeforces_practice/codeforces_0472A.java
@@ -0,0 +1,24 @@
+//https://codeforces.com/problemset/problem/472/A
+
+import java.util.Scanner;
+
+public class codeforces_0472A {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		switch (n % 3) {
+		case 0:
+			System.out.println(String.format("%d %d", 6, n - 6));
+			break;
+		case 1:
+			System.out.println(String.format("%d %d", 4, n - 4));
+			break;
+		case 2:
+			System.out.println(String.format("%d %d", 8, n - 8));
+			break;
+		}
+		scanner.close();
+	}
+
+}
