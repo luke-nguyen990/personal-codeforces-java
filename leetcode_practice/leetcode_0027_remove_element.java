44388283430bbb1aa1b2ad70e522e6dbf124d82c
Luke Nguyen <luke_nguyen@mckinsey.com>
2022-07-14 08:37:20 +0700

    leetcode-remove-element

---
+++ leetcode_practice/leetcode_0027_remove_element.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/remove-element/
+
+
+public class leetcode_0027_remove_element {
+
+	public int removeElement(int[] A, int elem) {
+		int m = 0;
+		for(int i = 0; i < A.length; i++) {
+			if(A[i] != elem) {
+				A[m++] = A[i];
+			}
+		}
+		return m;
+	}
+
+}
