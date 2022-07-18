36e1438a134e72670aad97c659603f2608ef1c99
Luke Nguyen <luke_nguyen@mckinsey.com>
2022-07-18 17:19:41 +0700

    codeforces - java

---
+++ codeforces_practice/codeforces_1360A.java
@@ -0,0 +1,23 @@
+//https://codeforces.com/problemset/problem/1360/A
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class codeforces_1360A {
+
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		for(int numCases = scanner.nextInt(); numCases > 0; --numCases) {
+			int l = scanner.nextInt(), h = scanner.nextInt();
+			if(l < h) {
+				int temp = l;
+				l = h; h = temp;
+			}
+			int squareSize = Math.max(l, h + h);
+			System.out.println((int)Math.pow(squareSize, 2));
+		}
+		scanner.close();
+	}
+
+}
