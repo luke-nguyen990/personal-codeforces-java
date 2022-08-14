46026c8b63fdebca5a6186b537f6e7554629353a
Luke Nguyen <luke_nguyen@mckinsey.com>
2022-08-14 11:07:25 +0700

    codeforces - java

--- codeforces_practice/codeforces_1678B2.java
+++ codeforces_practice/codeforces_1678B2.java
@@ -17,14 +17,25 @@ public class codeforces_1678B2 {
 		for (int numCases = scanner.nextInt(); numCases > 0; --numCases) {
 			int len = scanner.nextInt();
 			String s = scanner.next();
-			int numChange1 = 0;
-			int numChange2 = 0;
-			char last1 = '_';
-			char last2 = '_';
-			
-			for(int i = 0; i < len; i++) {
-				
+			int numSeg0 = 1;
+			int numSeg1 = 1;
+			int numChanges = 0;
+			for (int i = 0; i < len / 2; i++) {
+				int temp0 = numSeg0;
+				int temp1 = numSeg1;
+				if (s.charAt(i * 2) != s.charAt(i * 2 + 1)) {
+					numChanges++;
+					numSeg0 = Math.min(temp0, temp1 + 1);
+					numSeg1 = Math.min(temp1, temp0 + 1);
+				} else if (s.charAt(i * 2) == '0') {
+					numSeg0 = Math.min(temp0, temp1 + 1);
+					numSeg1 = len;
+				} else {
+					numSeg1 = Math.min(temp1, temp0 + 1);
+					numSeg0 = len;
+				}
 			}
+			writer.write(String.format("%d %d\n", numChanges, Math.min(numSeg0, numSeg1)));
 		}
 		scanner.close();
 		writer.flush();
